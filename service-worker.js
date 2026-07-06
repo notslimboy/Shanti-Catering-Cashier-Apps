ebc9ac6b09f714347a43268bd1a017d75c83c99a
notslimboy <notslimboy@users.noreply.github.com>
2026-07-06 08:40:27 +0700

    Refine modal header sizing

--- service-worker.js
+++ service-worker.js
@@ -1,8 +1,8 @@
-const CACHE_NAME = "kasir-bento-v242";
+const CACHE_NAME = "kasir-bento-v243";
 const APP_ASSETS = [
   "./",
   "./index.html",
-  "./styles.css?v=242",
+  "./styles.css?v=243",
   "./script.js?v=241",
   "./drivers/XP%20PRINTER%20DRIVER.rar",
   "./sample-items.csv",
